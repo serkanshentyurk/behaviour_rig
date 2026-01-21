a25f93e7685c008848e1f4d36c9a3a50ac7ffebb
Serkan Shentyurk <serkanshentyurk@hotmail.com>
2026-01-21 14:33:49 +0000

    test files added

---
+++ GUI/Launch_Bonsai_GUI_test.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+
+echo === STARTING SCRIPT ===
+
+REM --- Try activating Conda ---
+if exist "%USERPROFILE%\miniconda3\condabin\conda.bat" (
+    echo Found Miniconda
+    call "%USERPROFILE%\miniconda3\condabin\conda.bat" activate gui_env
+) else if exist "%USERPROFILE%\anaconda3\condabin\conda.bat" (
+    echo Found Anaconda
+    call "%USERPROFILE%\anaconda3\condabin\conda.bat" activate gui_env
+) else (
+    echo ERROR: Conda not found
+    goto end
+)
+
+echo Conda activation attempted
+echo Python location:
+where python
+python --version
+
+REM --- Move to script directory ---
+cd /d "%~dp0"
+echo Current directory: %CD%
+
+REM --- Run Python ---
+echo Running Python script...
+python "Bonsai_GUI_test.py"
+echo Python exited with code %ERRORLEVEL%
+
+:end
+echo.
+echo === PRESS ANY KEY TO CLOSE ===
+pause >nul
